1a6f81ac474bb6c22235070c5111519c0708158e
EricXRAY <147312721+EricXRAY@users.noreply.github.com>
2025-12-15 16:59:11 +0100

    Add toggleSkills function to manage skills display

---
+++ script.js
@@ -0,0 +1,11 @@
+<!-- ========================= script.js ========================= -->
+function toggleSkills() {
+const skills = document.getElementById('skills');
+
+
+if (skills.style.display === 'none') {
+skills.style.display = 'block';
+} else {
+skills.style.display = 'none';
+}
+}
